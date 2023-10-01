cdf3d97071c740db5ed8262f327f32d40bb7cc12
EVO Yeshu <evoyeshu@macbooks-MacBook-Air.local>
2023-10-01 20:04:52 +0530

    second_commit

---
+++ App/Navigation/route.js
@@ -0,0 +1,21 @@
+
+
+/**
+ *  File: route.js
+ *  Description: route file for the project
+ *  Auth: NA
+ *  Integration:NA
+ */
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function App(){
+    return(
+        <View>
+            <Text>
+                this is router page
+            </Text>
+        </View>
+    )
+}
--- android/app/src/main/java/com/school/MainActivity.java
+++ android/app/src/main/java/com/school/MainActivity.java
@@ -4,21 +4,28 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+// react-native-splash-screen >= 0.3.1
+import org.devio.rn.splashscreen.SplashScreen; // here
+
 
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule
    * rendering of the component.
    */
   @Override
   protected String getMainComponentName() {
+    SplashScreen.show(this); // here
     return "school";
   }
 
   /**
-   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
-   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util
+   * class {@link
+   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and
+   * Concurrent React
    * (aka React 18) with two boolean flags.
    */
   @Override
